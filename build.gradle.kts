60aee02e4d0c00bd38742a8615e58bc0f16db7e0
Hubert Kuliniak <kuliniakhubert@gmail.com>
2025-08-28 19:34:10 +0200

    bump libraries & gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import java.lang.String.format
 plugins {
     id("java")
     id("net.kyori.blossom") version "1.3.1"
-    id("com.gradleup.shadow") version "8.3.3"
-    id("com.palantir.git-version") version "3.1.0"
+    id("com.gradleup.shadow") version "8.3.9"
+    id("com.palantir.git-version") version "4.0.0"
 }
 
 project.group = "pl.mrstudios.essential"
@@ -17,7 +17,7 @@ val versionDetails: Closure<VersionDetails> by extra
 fun projectVersion(): String = format("%s (git/%s)", project.version, versionDetails().gitHash)
 
 java {
-    toolchain.languageVersion.set(of(21))
+    toolchain.languageVersion.set(of(23))
 }
 
 blossom {
@@ -35,48 +35,44 @@ repositories {
 dependencies {
 
     /* JDA */
-    implementation("net.dv8tion:JDA:${property("jda.version")}") {
-        exclude("opus-java")
-    }
+    implementation(libs.jda.core)
+    implementation(libs.jda.commands)
 
     /* Commons */
-    implementation("pl.mrstudios.commons:commons-sql:${property("commons.version")}")
+    implementation(libs.commons.sql)
 
     /* HikariCP */
-    implementation("com.zaxxer:HikariCP:${property("hikaricp.version")}")
+    implementation(libs.hikaricp)
 
     /* SQLite */
-    implementation("org.xerial:sqlite-jdbc:${property("xerial.sqlite.version")}")
+    implementation(libs.sqlite)
 
     /* Logback Classic */
-    implementation("ch.qos.logback:logback-classic:${property("logback.classic.version")}")
+    implementation(libs.logback.classic)
 
     /* Unirest */
-    implementation("com.konghq:unirest-java-core:${property("unirest.version")}")
-    implementation("com.konghq:unirest-modules-gson:${property("unirest.version")}")
+    implementation(libs.unirest.core)
+    implementation(libs.unirest.gson)
 
     /* Caffeine */
-    implementation("com.github.ben-manes.caffeine:caffeine:${property("caffeine.version")}")
-
-    /* Lite Commands */
-    implementation("dev.rollczi:litecommands-jda:${property("litecommands.version")}")
+    implementation(libs.caffeine)
 
     /* Okaeri Configs */
-    implementation("eu.okaeri:okaeri-configs-yaml-snakeyaml:${property("okaeri.configs.version")}")
+    implementation(libs.okaeri.configs)
 
     /* Rome */
-    implementation("com.rometools:rome:${property("rome.version")}")
+    implementation(libs.rome)
 
     /* Source Query */
-    implementation("com.ibasco.agql:agql-source-query:${property("source.query.version")}")
+    implementation(libs.source.query)
 
     /* Lombok */
-    compileOnly("org.projectlombok:lombok:${property("lombok.version")}")
-    annotationProcessor("org.projectlombok:lombok:${property("lombok.version")}")
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
 
     /* JetBrains Annotations */
-    compileOnly("org.jetbrains:annotations:${property("jetbrains.annotations.version")}")
-    annotationProcessor("org.jetbrains:annotations:${property("jetbrains.annotations.version")}")
+    compileOnly(libs.jetbrains.annotations)
+    annotationProcessor(libs.jetbrains.annotations)
 
 }
 
@@ -84,6 +80,7 @@ tasks {
 
     compileJava {
         options.encoding = "UTF-8"
+        options.compilerArgs.add("-parameters")
     }
 
     processResources {
