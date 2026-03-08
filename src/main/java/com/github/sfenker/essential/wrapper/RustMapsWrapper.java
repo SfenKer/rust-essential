670c5c2d8ae691052ca2223802f9103052a555f3
Hubert Kuliniak <kuliniakhubert@gmail.com>
2026-03-08 19:41:05 +0100

    server info command

--- src/main/java/com/github/sfenker/essential/wrapper/RustMapsWrapper.java
+++ src/main/java/com/github/sfenker/essential/wrapper/RustMapsWrapper.java
@@ -4,8 +4,11 @@ import kong.unirest.core.JsonNode;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.concurrent.CompletableFuture;
+
 import static java.lang.String.format;
-import static java.util.Optional.ofNullable;
+import static java.lang.System.getenv;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static kong.unirest.core.Unirest.get;
 
 public class RustMapsWrapper {
@@ -14,38 +17,32 @@ public class RustMapsWrapper {
         @NotNull Integer size,
         @NotNull Long seed
     ) {
-        return ofNullable(
-            get(format(API_ENDPOINT_MAP_BY_SEED, size, seed))
-                .header("X-API-Key", API_KEY)
-                .header("User-Agent", API_USER_AGENT)
-                .asJson().getBody()
-        ).map(JsonNode::getObject)
-
-            /* Status Code */
-            .filter((object) -> object.getJSONObject("meta").getInt("statusCode") == 200)
-
-            /* Data */
-            .filter((object) -> object.has("data"))
-            .map((object) -> object.getJSONObject("data"))
-
-            /* Image */
-            .filter((data) -> data.has("imageUrl"))
-            .map((data) -> data.getString("imageUrl"))
-
-            /* Or Else */
-            .orElse(null);
+        return get(format(API_ENDPOINT_MAP_BY_SEED, size, seed))
+            .header("X-API-Key", getenv("RUST_MAPS_API_KEY"))
+            .header("User-Agent", API_USER_AGENT)
+            .asJson()
+            .map(JsonNode::getObject)
+            .map(
+                (object) ->
+                    object.getJSONObject("data")
+            )
+            .mapBody(
+                (data) ->
+                    data.getString("imageUrl")
+            );
     }
 
-    private static @NotNull String API_KEY = "ENTER_API_KEY_HERE";
-    private static final @NotNull String API_BASE_URL = "https://api.rustmaps.com/v4/";
-    private static final @NotNull String API_USER_AGENT = "Rust Maps API Wrapper/1.0.0 (in: '{project}')";
-
-    private static final @NotNull String API_ENDPOINT_MAP_BY_SEED = API_BASE_URL + "maps/%d/%d?staging=false";
-
-    public static void provideRustMapsApiKey(
-        @NotNull String apiKey
+    public static @NotNull CompletableFuture<String> mapImageAsync(
+        @NotNull Integer size,
+        @NotNull Long seed
     ) {
-        API_KEY = apiKey;
+        return supplyAsync(() -> mapImage(size, seed));
     }
+
+    static final @NotNull String API_BASE_URL = "https://api.rustmaps.com/v4/";
+    static final @NotNull String API_USER_AGENT = "Rust Maps API Wrapper/1.0.0 (in: '{project}')";
+
+    static final @NotNull String API_ENDPOINT_MAP_BY_SEED =
+        API_BASE_URL + "maps/%d/%d?staging=false";
 
 }
